fb7cc726e6bd9e234587863698205fae007324d4
Alex <Oleksandr.Piatkin@gmx.com>
2020-03-27 18:38:27 +0100

    task5-A v1.1

---
+++ script/array_of_products.js
@@ -0,0 +1,210 @@
+const products = [
+    {
+        name: 'Margherita',
+        ingredients: ["Tomato sauce", "mozzarella", "oregano"],
+    },
+    {
+        name: 'Marinara',
+        ingredients: ["Tomato sauce", "garlic", "basil"],
+    },
+    {
+        name: 'Quattro Stagioni',
+        ingredients: ["Tomato sauce", "mozzarella", "mushrooms", "ham", "artichokes", "olives", "oregano"],
+    },
+    {
+        name: 'Carbonara',
+        ingredients: ["Tomato sauce", "mozzarella", "parmesan", "eggs", "bacon"],
+    },
+    {
+        name: 'Frutti di Mare',
+        ingredients: ["Tomato sauce", "seafood"],
+    },
+    {
+        name: 'Quattro Formaggi',
+        ingredients: ["Tomato sauce", "mozzarella", "parmesan", "gorgonzola", "artichokes", "oregano"],
+    },
+    {
+        name: 'Crudo',
+        ingredients: ["Tomato sauce", "mozzarella", "Parma ham"],
+    },
+    {
+        name: 'Napoli',
+        ingredients: ["Tomato sauce", "mozzarella", "oregano", "anchovies"],
+    },
+    {
+        name: 'Pugliese',
+        ingredients: ["Tomato sauce", "mozzarella", "oregano", "onions"],
+    },
+    {
+        name: 'Montanara',
+        ingredients: ["Tomato sauce", "mozzarella", "mushrooms", "pepperoni", "soft cheese"],
+    },
+    {
+        name: 'Emiliana',
+        ingredients: ["Tomato sauce", "mozzarella", "eggplant", "boiled potatoes", "sausage"],
+    },
+    {
+        name: 'Romana',
+        ingredients: ["Tomato sauce", "mozzarella", "anchovies", "capers", "oregano"],
+    },
+    {
+        name: 'Fattoria',
+        ingredients: ["Tomato sauce", "mozzarella", "peppers", "peas", "porchetta"],
+    },
+    {
+        name: 'Schiacciata',
+        ingredients: ["Olive oil", "rosemary"],
+    },
+    {
+        name: 'Prosciutto',
+        ingredients: ["Tomato sauce", "mozzarella", "ham", "oregano"],
+    },
+    {
+        name: 'Prosciutto e Funghi',
+        ingredients: ["Tomato sauce", "mozzarella", "ham", "mushrooms"],
+    }
+];
+const ingredientsData = [ 
+    {
+        name: "Tomato sauce",
+        price: 2,
+        calories: 33
+    },
+    {
+        name: "mozzarella",
+        price: 4,
+        calories: 242
+    },
+    {
+        name: "oregano",
+        price: 4,
+        calories: 10
+    },
+    {
+        name: "garlic",
+        price: 1,
+        calories: 10
+    },
+    {
+        name: "basil",
+        price: 2,
+        calories: 8
+    },
+    {
+        name: "mushrooms",
+        price: 4,
+        calories: 46
+    },
+    {
+        name: "ham",
+        price: 6,
+        calories: 270
+    },
+    {
+        name: "artichokes",
+        price: 5,
+        calories: 14
+    },
+    {
+        name: "olives",
+        price: 3,
+        calories: 21
+    },
+    {
+        name: "parmesan",
+        price: 4,
+        calories: 122
+    },
+    {
+        name: "eggs",
+        price: 2,
+        calories: 75
+    },
+    {
+        name: "bacon",
+        price: 7,
+        calories: 310
+    },
+    {
+        name: "seafood",
+        price: 12,
+        calories: 291
+    },
+    {
+        name: "gorgonzola",
+        price: 8,
+        calories: 190
+    },
+    {
+        name: "Parma ham",
+        price: 7,
+        calories: 120
+    },
+    {
+        name: "anchovies",
+        price: 6,
+        calories: 60
+    },
+    {
+        name: "onions",
+        price: 1,
+        calories: 21
+    },
+    {
+        name: "pepperoni",
+        price: 4,
+        calories: 7
+    },
+    {
+        name: "soft cheese",
+        price: 11,
+        calories: 190
+    },
+    {
+        name: "eggplant",
+        price: 5,
+        calories: 12
+    },
+    {
+        name: "boiled potatoes",
+        price: 2,
+        calories: 31
+    },
+    {
+        name: "sausage",
+        price: 9,
+        calories: 290
+    },
+    {
+        name: "capers",
+        price: 4,
+        calories: 5
+    },
+    {
+        name: "peppers",
+        price: 4,
+        calories: 7
+    },
+    {
+        name: "peas",
+        price: 6,
+        calories: 42
+    },
+    {
+        name: "porchetta",
+        price: 9,
+        calories: 215
+    },
+    {
+        name: "Olive oil",
+        price: 3,
+        calories: 70
+    },
+    {
+        name: "rosemary",
+        price: 5,
+        calories: 14
+    }
+];
+
+
--- script/main.js
+++ script/main.js
@@ -1,17 +1,49 @@
+const root = document.getElementById('root');
+
 let arr = [];
 
-let i = 0;
+const coppyArray = function (arr) {
+    return arr.slice();
+};
+
+const fillProductsArr = function () {
+    let i = 0;
+    while (i < products.length) {
+        const pizza = new PizzaCard(products[i], ingredientsData);
+        i++;
+        arr.push(pizza);
+    }
+};
+
+const drowProducts = function () {
+    arr.forEach(pizza => pizza.createProductCard());
+};
+
+const parametersCalculation = function (ingredients) {
+    // pizza dough price & calories
+    let countedPrice = 5;
+    let countedCalories = 250;
+
+    ingredients.forEach((item) => {
+        const ingredient = item;
+
+        ingredientsData.forEach((element) => {
+            if (element.name == ingredient) {
+                countedPrice += element.price;
+                countedCalories += element.calories;
+            }
+        })
+    })
+    return [countedPrice, countedCalories];
+};
+
 class PizzaCard {
-    constructor() {
-        this.name = products[i].name;
-        this.ingredients = products[i].ingredients;
+    constructor({ name, ingredients }, ingredientsData) {
+        this.name = name;
+        this.ingredients = ingredients;
         this.parameters = parametersCalculation(this.ingredients);
         this.price = this.parameters[0];
         this.calories = this.parameters[1];
-        this.canvasCode = this.parameters[2];
-
-        arr.push(this);
-        i++;
     }
 
     createProductCard() {
@@ -19,7 +51,7 @@ class PizzaCard {
         const article = document.createElement('article');
         root.appendChild(article);
 
-        const canvas = document.createElement("CANVAS");
+        const canvas = document.createElement("canvas");
         const ctx = canvas.getContext("2d");
         ctx.beginPath();
         ctx.arc(100, 75, 75, 0, 2 * Math.PI, true);
@@ -35,106 +67,196 @@ class PizzaCard {
         header.innerHTML = this.name;
         article.appendChild(header);
 
-        const description = document.createElement('p');
-        const ingredientsStr = 'Ingredients: ' + this.ingredients.join(', ') + '.';
-        description.innerHTML = ingredientsStr;
+        const description = document.createElement('ul');
         article.appendChild(description);
 
-        const description2 = document.createElement('p');
-        const caloriesStr = 'Calories: ' + this.calories + ' kcal';
-        description2.innerHTML = caloriesStr;
-        article.appendChild(description2);
+        const ingredientsD = document.createElement('li');
+        const ingredientsStr = `Ingredients: ${this.ingredients.join(', ')}.`;
+        ingredientsD.innerHTML = ingredientsStr;
+        description.appendChild(ingredientsD);
 
-        const description3 = document.createElement('span');
-        const priceStr = 'Price: ' + this.price;
-        description3.innerHTML = priceStr;
-        article.appendChild(description3);
+        const caloriesD = document.createElement('li');
+        const caloriesStr = `Calories: ${this.calories} kcal`;
+        caloriesD.innerHTML = caloriesStr;
+        description.appendChild(caloriesD);
+
+        const priceD = document.createElement('li');
+        const priceStr = `Price: ${this.price}`;
+        priceD.innerHTML = priceStr;
+        description.appendChild(priceD);
     }
 }
 
-//View changing part
-gridViewBtn.onclick = function gridView() {
+fillProductsArr();
+let copiedArrey = coppyArray(arr);
+
+//Products view changing
+const gridViewBtn = document.getElementById('grid_button');
+const listViewBtn = document.getElementById('list_button');
+
+const switchViewType = function () {
+    gridViewBtn.classList.toggle('on');
+    listViewBtn.classList.toggle('on');
+}
+
+const gridView = function () {
     if (gridViewBtn.className == "switch-button") {
-        gridViewBtn.className = "switch-button on";
-        listViewBtn.className = "switch-button";
-        const labledStyle = document.getElementById("delete_lable");
-        labledStyle.remove();
+        switchViewType();
+        root.classList.remove('list_view');
     }
 };
 
-listViewBtn.onclick = function listView() {
+const listView = function () {
     if (listViewBtn.className == "switch-button") {
-        gridViewBtn.className = "switch-button";
-        listViewBtn.className = "switch-button on";
-
-        const style = document.createElement('style');
-        style.id = "delete_lable";
-        style.innerHTML = listStyle;
-        document.head.appendChild(style);
+        switchViewType();
+        root.classList.add('list_view');
     }
 };
 
-//Filter part
-nameFilterBtn.onclick = function nameFilter() {
-    if (nameFilterBtn.className == "switch-filter") {
-        nameFilterBtn.className = "switch-filter on";
-        priseFilterBtn.className = "switch-filter";
+listViewBtn.addEventListener('click', listView);
+gridViewBtn.addEventListener('click', gridView);
 
-        let filtredByName = coppyArrey(arr).sort((a, b) => {
+//Products view
+const mainManuBtn = document.getElementById('main_page_btn');
+const productsManuBtn = document.getElementById('products_page_btn');
+const mainImgBtn = document.getElementById('img_btn_main');
+const startContent = document.getElementById('content_start');
+const contentProductsControl = document.getElementById('content_products_control');
+const contentChoose = document.getElementById('content_choose');
+const h1 = document.getElementById('h_1');
 
-            const nameA = a.name.toLowerCase(); 
-            const nameB = b.name.toLowerCase();
+let firstView = true;
 
-            if (nameA < nameB) { return -1; }
-            if (nameA > nameB) { return 1; }
-            return 0;
+const productsCall = function () {
 
-        });
+    const productsOn = function () {
+        startContent.classList.add('hidden');
+        h1.classList.add('hidden');
+        productsManuBtn.classList.add('on');
+        mainManuBtn.classList.remove('on');
+    };
 
-        root.innerHTML = '';
-        filtredByName.forEach(pizza => pizza.createProductCard());
-
+    if (firstView) {
+        contentChoose.classList.remove('hidden');
+        productsOn();
+        firstView = false;
     } else {
-        nameFilterBtn.className = "switch-filter";
-        root.innerHTML = '';
-        drowProducts();
+        if (mainManuBtn.classList == 'on') {
+            root.classList.remove('hidden');
+            productsOn();
+            contentProductsControl.classList.remove('hidden');
+        }
     }
 };
+mainImgBtn.addEventListener('click', productsCall);
+productsManuBtn.addEventListener('click', productsCall);
 
-priseFilterBtn.onclick = function priseFilter() {
-    if (priseFilterBtn.className == "switch-filter") {
-        priseFilterBtn.className = "switch-filter on";
-        nameFilterBtn.className = "switch-filter";
 
-        let filtredByPrice = coppyArrey(arr).sort((a, b) => {
 
-            const priceA = a.price;
-            const priceB = b.price;
+const mainCall = function () {
+    if (mainManuBtn.className == '') {
+        mainManuBtn.className = 'on';
+        productsManuBtn.className = '';
+        startContent.classList.remove('hidden');
+        h1.classList.remove('hidden');
+        root.classList.add('hidden');
+        contentProductsControl.classList.add('hidden');
+    }
 
-            return priceA - priceB;
+};
+mainManuBtn.addEventListener('click', mainCall);
 
-        });
-        console.log('filtred By price\n', filtredByPrice);
+const chooseGridBtn = document.getElementById('choose_grid');
+const chooseListBtn = document.getElementById('choose_list');
+
+const chooseGrid = function () {
+    contentChoose.classList.add('hidden');
+    contentProductsControl.classList.remove('hidden');
+    root.classList.remove('hidden');
+    root.innerHTML = '';
+    drowProducts();
+};
+
+const chooseList = function () {
+    root.classList.add('list_view');
+    chooseGrid();
+    gridViewBtn.classList.remove('on');
+    listViewBtn.classList.add('on');
+};
+
+chooseListBtn.addEventListener('click', chooseList);
+chooseGridBtn.addEventListener('click', chooseGrid);
+
+//Filter part ------>
+const nameFilterBtn = document.getElementById('name_filter_button');
+const priceFilterBtn = document.getElementById('prise_filter_button');
+
+
+const filtredByName = function () {
+    const filtr = coppyArray(copiedArrey).sort((a, b) => {
+        const nameA = a.name.toLowerCase();
+        const nameB = b.name.toLowerCase();
+        if (nameA < nameB) { return -1; }
+        if (nameA > nameB) { return 1; }
+        return 0;
+    });
+    return filtr;
+}
+
+const filtredByPrice = function () {
+    const filtr = coppyArray(copiedArrey).sort((a, b) => {
+        return a.price - b.price;
+    });
+    return filtr;
+}
+
+const filterFunc = function (buttonOn, buttonOff, filter) {
+
+    if (buttonOn.className == 'switch-filter') {
+        buttonOn.classList.add('on');
+        buttonOff.classList.remove('on');
         root.innerHTML = '';
-        filtredByPrice.forEach(pizza => pizza.createProductCard());
-
+        filter().forEach(pizza => pizza.createProductCard());
     } else {
-        priseFilterBtn.className = "switch-filter";
         root.innerHTML = '';
-        drowProducts();
+        buttonOn.classList.remove('on');
+        copiedArrey.forEach(pizza => pizza.createProductCard());
     }
 };
+const filterFuncName = function () { filterFunc(nameFilterBtn, priceFilterBtn, filtredByName); };
+const filterFuncPrice = function () { filterFunc(priceFilterBtn, nameFilterBtn, filtredByPrice); };
+
+priceFilterBtn.addEventListener('click', filterFuncPrice);
+nameFilterBtn.addEventListener('click', filterFuncName);
 
 // search by ingredients Form
-searchForm.onsubmit = function () {
-    const searchRequest = headerSearchInp.value;
-    console.log('search req', searchRequest);
-    headerSearchInp.value = '';
-    root.innerHTML = '';
-    rootMainPage.innerHTML = '';
+const headerSearchInp = document.getElementById('header_search');
+const searchForm = document.getElementById('search_form');
+const errorMessage = document.getElementById("error");
 
-    let filtredBySearch = [];
-    coppyArrey(arr).forEach((element) => {
+const filterForSearch = function () {
+    if (nameFilterBtn.className == 'switch-filter on') {
+        nameFilterBtn.className.remove('on');
+        filterFuncName();
+    } else if ( priceFilterBtn.className == 'switch-filter on') {
+        priceFilterBtn.className.remove('on');
+        filterFuncPrice();
+    } else {
+        copiedArrey.forEach(pizza => pizza.createProductCard());
+    }
+};
+
+searchForm.onsubmit = function () {
+    firstView = false;
+    if (mainManuBtn.className == 'on') { productsCall(); }
+    if (errorMessage.classList == "error") {
+        errorMessage.classList.add("hidden");
+    }
+    const searchRequest = headerSearchInp.value;
+    root.innerHTML = '';
+    copiedArrey = [];
+
+    coppyArray(arr).forEach((element) => {
 
         if (element.ingredients.reduce(function (result, name) {
             const searchLower = searchRequest.toLowerCase();
@@ -144,30 +266,24 @@ searchForm.onsubmit = function () {
             return result + check;
         }, 0)
         ) {
-            filtredBySearch.push(element);
-            console.log(element);
+            copiedArrey.push(element);
         }
     });
 
-    if (nameFilterBtn.className == "switch-filter on") {
-
-        filtredBySearch.sort((a, b) => {
-            const getA = a.name;
-            const getB = b.name;
-            if (getA[0] < getB[0]) { return -1; }
-            if (getA[0] > getB[0]) { return 1; }
-            return 0;
-        });
+    if (copiedArrey.length != 0) {
+        filterForSearch();
+    } else {
+        errorMessage.classList.remove("hidden");
     }
-    if (priseFilterBtn.className == "switch-filter on") {
-
-        filtredBySearch.sort((a, b) => {
-            const priceA = a.price;
-            const priceB = b.price;
-            return priceA - priceB;
-        });
-    }
-
-    root.innerHTML = '';
-    filtredBySearch.forEach(pizza => pizza.createProductCard());
 };
+
+// reset search
+const resetBtn = document.getElementById('reset_button');
+const resetSearchItems = function () {
+    copiedArrey = coppyArray(arr);
+    root.innerHTML = '';
+    nameFilterBtn.className = 'switch-filter';
+    priceFilterBtn.className = 'switch-filter';
+    drowProducts();
+}
+resetBtn.addEventListener('click', resetSearchItems);
